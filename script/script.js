eea3ca9ef24366c90279870c966f3fc8e94bef4a
Varun Bhabhra <75928040+Varun-Bhabhra@users.noreply.github.com>
2022-02-13 09:38:40 +0530

    About me & experience section added

--- script/script.js
+++ script/script.js
@@ -1,6 +1,4 @@
 ﻿const hamburger = document.querySelector('#check');
 const dropdown = document.querySelector('#dropdown');
 
-hamburger.addEventListener('click', () => {
-  dropdown.classList.toggle('hidden');
-});
+hamburger.addEventListener('click', () => dropdown.classList.toggle('hidden'));
