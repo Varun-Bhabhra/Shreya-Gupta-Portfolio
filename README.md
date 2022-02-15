e560188b15b052b6e161fc21a4ac0a5c5c91fdf1
Varun Bhabhra <75928040+Varun-Bhabhra@users.noreply.github.com>
2022-02-15 18:30:29 +0530

    New animations

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A portfolio website for a MSc Computer Science student, which happens to be my f
 
 # Color Palatte
 
-1. Primary #023047 [Navy Blue]
+1. Primary #232734 [Navy Blue]
 2. Secondary #64ffda [Neon Blue]
 3. Text Dark #94a3b8 [Dark Gray]
 4. Text Dark #caf0f8 [Light Blue]
--- script/script.js
+++ script/script.js
@@ -1,4 +1,28 @@
-﻿const hamburger = document.querySelector('#check');
+﻿// VARIABLE DECLARATION
+const hamburger = document.querySelector('#check');
 const dropdown = document.querySelector('#dropdown');
 
+const about = document.querySelectorAll('.about');
+const experience = document.querySelectorAll('.experience');
+const work = document.querySelectorAll('.work');
+const contact = document.querySelectorAll('.contact');
+
+const landing_section = document.querySelector('#landing_section');
+const about_section = document.querySelector('#about_section');
+const experience_section = document.querySelector('#experience_section');
+const contact_section = document.querySelector('#contact_section');
+
+
 hamburger.addEventListener('click', () => dropdown.classList.toggle('hidden'));
+
+for (let el of about) {
+  el.addEventListener('click', () => about_section.scrollIntoView({ behavior: 'smooth' }));
+}
+
+for (let el of experience) {
+  el.addEventListener('click', () => experience_section.scrollIntoView({ behavior: 'smooth' }));
+}
+
+for (let el of contact) {
+  el.addEventListener('click', () => contact_section.scrollIntoView({ behavior: 'smooth' }));
+}
